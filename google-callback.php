99960f9043c0b7dffd972f5e99fe34ea14819ff3
Khuzghadi <ghadiyalikhuzaima169@gmail.com>
2025-11-08 01:30:30 +0530

    Auto-commit on save

--- google-callback.php
+++ google-callback.php
@@ -1,27 +1,30 @@
 <?php
 session_start();
+require_once __DIR__ . '/db.php';
 require_once __DIR__ . '/google-api-php-client/vendor/autoload.php';
-require_once 'db.php';
 
 $client = new Google_Client();
 $client->setClientId('893942546879-32236p7a4defodh60gu72hhbuhf9dqtc.apps.googleusercontent.com');
 $client->setClientSecret('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');
 $client->setRedirectUri('http://localhost/NZ-IMS/google-callback.php');
-$client->addScope('email');
-$client->addScope('profile');
 
 if (isset($_GET['code'])) {
     $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+
     if (!isset($token['error'])) {
         $client->setAccessToken($token['access_token']);
-        $google_service = new Google_Service_Oauth2($client);
-        $data = $google_service->userinfo->get();
 
-        $email = $data['email'];
-        $name = $data['name'];
-        $picture = $data['picture'];
+        $oauth = new Google_Service_Oauth2($client);
+        $google_user = $oauth->userinfo->get();
 
-        $query = $conn->prepare("SELECT * FROM zonal_head WHERE email=?");
+        $email = $google_user->email;
+        $name = $google_user->name;
+        $picture = $google_user->picture;
+
+        $query = $conn->prepare("SELECT * FROM users WHERE email=?");
+        if (!$query) {
+            die("Prepare failed: " . $conn->error);
+        }
         $query->bind_param("s", $email);
         $query->execute();
         $result = $query->get_result();
@@ -30,17 +33,25 @@ if (isset($_GET['code'])) {
             $_SESSION['email'] = $email;
             $_SESSION['name'] = $name;
             $_SESSION['picture'] = $picture;
-            header('Location: frame.php');
-            exit();
         } else {
-            echo "Access denied: This Gmail is not registered by admin.";
-            exit();
+            $insert = $conn->prepare("INSERT INTO users (name, email, picture) VALUES (?, ?, ?)");
+            if (!$insert) {
+                die("Prepare failed: " . $conn->error);
+            }
+            $insert->bind_param("sss", $name, $email, $picture);
+            $insert->execute();
+
+            $_SESSION['email'] = $email;
+            $_SESSION['name'] = $name;
+            $_SESSION['picture'] = $picture;
         }
+
+        header('Location: frame.php');
+        exit();
     } else {
-        echo "Error fetching token: " . $token['error_description'];
+        echo "Error fetching token: " . $token['error'];
     }
 } else {
-    header('Location: login.php');
-    exit();
+    echo "No authorization code returned.";
 }
 ?>
